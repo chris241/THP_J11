2d514738466770deef3d716fa2c9a0df97e804ea
chris <chrisleon325@gmail.com>
2019-07-24 16:11:11 +0300

    maj2

--- app.rb
+++ app.rb
@@ -4,18 +4,36 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-player1 = Player.new("José")
+player1 = Player.new("Kin")
 puts "#{player1.name} vous avez #{player1.life_points} points"
-puts player1.show_state
-puts player1.gets_damage(5)
-puts player1.show_state
-puts player1.gets_damage(2)
-puts player1.show_state
-puts player1.gets_damage(3)
-puts player1.show_state
-
-
 player2 = Player.new("Chris")
 puts "#{player2.name} vous avez #{player2.life_points} points"
 
-puts "Nombre de joueur actuelle #{Player.count}"
+puts "-----------------------------------------------------"
+puts "-----------------------------------------------------"
+puts "-----------------------------------------------------"
+puts "--------------Nombre de joueurs #{Player.count}--------------------"
+puts "-----------------------------------------------------"
+puts "--------------------BEGIN----------------------------"
+puts "-----------------------------------------------------"
+
+while player1.life_points > 0 && player2.life_points > 0
+	puts "*********Voici l'état de chaque joueurs************"
+	puts "#{player1.show_state}"
+	puts "#{player2.show_state}"
+	puts "*********Passons à la phase d'attaque*************"
+	puts player1.attacks(player2)
+	puts player2.show_state
+	if player2.life_points <= 0
+		puts "#{player2.name} a été tué par #{player1.name}"
+		 puts "#{player1.name} remporte le combat"
+        break
+    end
+puts player2.attacks(player1)
+puts player1.show_state
+    if player1.life_points <= 0
+        puts "#{player1.name} a été tué par #{player2.name}"
+        puts "#{player2.name} remporte le combat"
+        break
+    end
+end
--- lib/player.rb
+++ lib/player.rb
@@ -1,23 +1,41 @@
 class Player
-	attr_accessor :name, :life_points, :sous
+
+	attr_accessor :name, :life_points
 	@@count = 0
-	def initialize(name, life_points = 10)
-		@name= name
-		@life_points = life_points 
+	def initialize(player)
+		@name= player
+		@life_points = 10
 		@@count += 1
 	end
+
+	#Affiche  le nombre de vite du joueur 
 	def show_state
 		puts "#{name} a #{life_points} points de vie"
 	end
 
-	def gets_damage(sous)
-		@sous = sous
-		@life_points -= @sous
-		if @life_points == 0
-			puts "****************le joureur #{name} a été tué!!!*********************"
-		end
-	end
 	def self.count
 		return @@count
 	end
+
+	def gets_damage(damage_received)
+		@life_points -= damage_received
+		if @life_points <= 0
+			puts "Le joureur #{@name} a été tué"
+		end
+	end
+
+	# Dommage qu'on inflige à l'autre joueur
+	def compute_damage
+		return rand(1..6)
+	end
+	
+	# Attaque sur l'autre joueur
+	def attacks (player)
+		puts "le joueur #{@name} attaque le joueur #{player.name}"
+		damage = compute_damage
+		puts "Il lui inflige #{damage} points de dommages"
+		player.gets_damage(damage)
+
+	end
+	
 end
